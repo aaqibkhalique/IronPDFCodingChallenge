023e1b65d88b4a909c2f66fc86ef17cc3cf19e96
Aaqib Khalique <aaqibkhaliq2@gmail.com>
2025-03-18 08:09:27 +0500

    Add project files.

---
+++ IronPDFCodingChallenge/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronPDFCodingChallenge
+{
+    public class Phone
+    {
+
+        public static String OldPhonePad(string input)
+        {
+
+
+            return input;
+        }
+
+    }
+}
---
+++ IronPDFCodingChallenge/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using IronPDFCodingChallenge;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Phone.OldPhonePad("test"));
+
+
+Console.ReadLine();
