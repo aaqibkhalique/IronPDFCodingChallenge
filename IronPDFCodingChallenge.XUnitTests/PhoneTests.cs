3144b2abf0626dcbb5513cdd94bbebcac36594f2
Aaqib Khalique <aaqibkhaliq2@gmail.com>
2025-04-03 01:35:44 +0500

    added new test cases

--- IronPDFCodingChallenge.XUnitTests/PhoneTests.cs
+++ IronPDFCodingChallenge.XUnitTests/PhoneTests.cs
@@ -16,6 +16,59 @@
             Assert.Equal(expected, result);
         }
 
+        [Theory]
+        [InlineData("2#", "A")]
+        [InlineData("7777#", "S")]
+        [InlineData("0#", " ")]
+        public void Test_SingleKeyPress_ReturnsCorrectOutput(string input, string expected)
+        {
+            // Act
+            string result = Phone.OldPhonePad(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("999 33 7777#", "YES")]
+        [InlineData("66 666#", "NO")]
+        [InlineData("666 55 2 999#", "OKAY")]
+        public void Test_RepeatedKeyPresses_ReturnsCorrectOutput(string input, string expected)
+        {
+            // Act
+            string result = Phone.OldPhonePad(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("2 22*#", "A")]
+        [InlineData("777* 22 2#", "BA")]
+        [InlineData("77**#", "")]
+        public void Test_BackspaceHandling_RemovesPreviousCharacter(string input, string expected)
+        {
+            // Act
+            string result = Phone.OldPhonePad(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("123", "")]
+        [InlineData("**#", "")]
+        [InlineData("****#", "")]
+        [InlineData("2*3*#", "")]
+        public void Test_InvalidInputs_ReturnsEmptyString(string input, string expected)
+        {
+            // Act
+            string result = Phone.OldPhonePad(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
 
     }
