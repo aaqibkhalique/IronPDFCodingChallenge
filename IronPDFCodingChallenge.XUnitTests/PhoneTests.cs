c20413ca62b81aee2bff2a6316afcdf644324ad4
Aaqib Khalique <aaqibkhaliq2@gmail.com>
2025-04-03 01:13:30 +0500

    added Tests with input decorator

--- IronPDFCodingChallenge.XUnitTests/PhoneTests.cs
+++ IronPDFCodingChallenge.XUnitTests/PhoneTests.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IronPDFCodingChallenge.XUnitTests
+﻿namespace IronPDFCodingChallenge.XUnitTests
 {
-    internal class PhoneTests
+    public class PhoneTests
     {
+        [Theory]
+        [InlineData("2 2 77 444 22#", "AAQIB")]
+        [InlineData("227*#", "B")]
+        [InlineData("4433555 555666#", "HELLO")]
+        [InlineData("8 88777444666*664#", "TURING")]
+        public void Test_Input_ReturnsCorrectOutput(string input, string expected)
+        {
+            // Act
+            string result = Phone.OldPhonePad(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+
+
     }
 }
